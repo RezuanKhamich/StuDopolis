b39ebfe67edf73f584f3f0fb26a459c3f326f488
Rezuan Khamukov <r.hamukov@chatme.ai>
2022-11-17 22:11:44 +0600

    Fix 5

--- src/component/Model/Scene.js
+++ src/component/Model/Scene.js
@@ -44,6 +44,7 @@ export function Scene (props) {
             ))
           }
 
+          {/*asdas*/}
 
           {/*<group position={[-1.47, 2.9, -1.08]}>*/}
           {/*  <mesh geometry={nodes.Object_16.geometry} material={materials.shower_light} />*/}
